0ebfd550baa55f30f5b2c835f9f6ad7c9bfd340b
E10N11 <144256512+E10N11@users.noreply.github.com>
2024-09-06 15:30:02 +0000

    Merge to Fill Work

--- Person.java
+++ Person.java
@@ -7,10 +7,10 @@ public class Person {
   //string fields
   private String firstName;
   private String lastName;
-  private Int Id;
+  private int Id;
 
   //constructor
-  public person(String firstName, String lastName, Int id){
+  Person(String fname, String lname, int id){
     firstName = fname;
     lastName = lname;
     Id = id;
@@ -18,13 +18,13 @@ public class Person {
 
 //Accessor methods
   public String getFirstName(){
-    return fname;
+    return firstName;
   }
   public String getLastName(){
-    return lname;
+    return lastName;
   }
-  public String getID(){
-    return id;
+  public int getId(){
+    return Id;
   }
 
   //to string method
@@ -33,59 +33,58 @@ public class Person {
   }
 }
 
-public class MyMain {
-
+class MyMain {
+  /* 
     public static void store (InputStream istream, LinkedList<Person> list)
     {
-        BufferedReader reader = new BufferedReader (newInputStreamReader (istream));
-        string line;
+        BufferedReader reader = new BufferedReader (new FileReader ("data.txt"));
+        String line;
         while ((line = reader.readLine()) != null) 
         {
-            string[] parts = line.split(",");
-            list.add(new Person(fname, lname, id));
+            String[] parts = line.split(",");
+            list.add(new Person(parts[0], parts[1], Integer.parseInt(parts[2])));
         }
+        reader.close();
     }
 
     public static void display (PrintStream outputStream, LinkedList<Person> list)
     {
-        for (int i = 0; i < linkedList.size(); i++) { 
-            outputStream.print(linkedList.get(i).toString() + "/n"); 
+        for (int i = 0; i < list.size(); i++) { 
+            outputStream.print(list.get(i).toString() + "/n"); 
         }
     }
-
-    public static int find (String sid, LinkedList<Person> list) 
+        */
+    public static int find (int sid, LinkedList<Person> list) 
     {
         for (int i=0; i < list.size(); i++)
         {
-            if (list.get(i).getID().equals(sid)) 
+            if (list.get(i).getId() == sid) 
             {
                 return i;
             }
         }
         return -1;
     }
-
     public static void main(String[] args) {
-        LinkedList<Person> personList = new LinkedList<>();
-
-        String dataFilePath = "data.txt";
-        try (InputStream inputStream = new FileInputStream(dataFilePath)) {
-            store(inputStream, personList);
-        } catch (IOException e) {
-            System.out.println("Error reading data file: " + e.getMessage());
+      LinkedList<Person> personList = new LinkedList<>();
+      /* 
+      String dataFilePath = "data.txt";
+      try (InputStream inputStream = new FileInputStream(dataFilePath)) {
+          store(inputStream, personList);
+      } catch (IOException e) {
+          System.out.println("Error reading data file: " + e.getMessage());
+      }
+      */
+      System.out.println("Displaying all persons: /n");
+      //display(System.out, personList);
+      int[] testIds = {12345, 67890, 11111};
+      for (int id : testIds) {
+        int index = find(id, personList);
+        if (index != -1) {
+            System.out.println("Person with ID " + id + " found at index " + index + ": " + personList.get(index));
+        } else {
+            System.out.println("Person with ID " + id + " not found.");
         }
-
-        System.out.println("Displaying all persons: /n");
-        display(System.out, personList);
-
-        String[] testIds = {"12345", "67890", "11111"};
-        for (String id : testIds) {
-            int index = find(id, personList);
-            if (index != -1) {
-                System.out.println("Person with ID " + id + " found at index " + index + ": " + personList.get(index));
-            } else {
-                System.out.println("Person with ID " + id + " not found.");
-            }
-        }
+      }
     }
 }
